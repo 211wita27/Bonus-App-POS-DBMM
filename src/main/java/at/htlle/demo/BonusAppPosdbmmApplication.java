cda5ce7319aa61fecc5a2e7aaa6dd916c6a3e5d0
Mario Ziegerhofer <132365562+211wita27@users.noreply.github.com>
2025-09-23 11:36:35 +0200

    Added spring boot project

---
+++ src/main/java/at/htlle/demo/BonusAppPosdbmmApplication.java
@@ -0,0 +1,13 @@
+package at.htlle.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonusAppPosdbmmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BonusAppPosdbmmApplication.class, args);
+	}
+
+}
